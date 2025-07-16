ff6adbfbee827bb9771a0cecbbfd33cba58fb0ed
Patrick Lafrance <patricklafrance@users.noreply.github.com>
2025-07-16 15:19:16 -0400

    docs: Updated dependencies (#295)

--- packages/firefly-honeycomb/src/tracer.ts
+++
@@ -1,6 +0,0 @@
-import { trace } from "@opentelemetry/api";
-
-export function getTracer() {
-    // The tracer name is used as the "library.name" attribute.
-    return trace.getTracer("@squide/honeycomb");
-}
