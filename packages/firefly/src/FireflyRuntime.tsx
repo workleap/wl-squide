33a7b4f183b70c0bc1472c67b0f6e91801dd8aab
patricklafrance <pa.lafrance@gmail.com>
2025-09-05 15:55:44 -0400

    refactor(runtime): remove unused interfaces and class definitions from runtime files

--- packages/firefly/src/FireflyRuntime.tsx
+++ packages/firefly/src/FireflyRuntime.tsx
@@ -10,125 +10,6 @@ export interface FireflyRuntimeOptions extends RuntimeOptions {
     useMsw?: boolean;
 }
 
-// export interface FireflyRuntimeMembers extends ReactRouterRuntimeMembers {
-//     appRouterStore: AppRouterStore;
-//     useMsw: boolean;
-// }
-
-// const fireflyRuntimeMembersKeys: (keyof FireflyRuntimeMembers)[] = [
-//     "appRouterStore",
-//     "useMsw"
-// ];
-
-// export function isFireflyRuntimeMembers(obj: unknown): obj is FireflyRuntimeMembers {
-//     if (obj && typeof obj === "object") {
-//         return isReactRouterRuntimeMembers(obj) && fireflyRuntimeMembersKeys.every(
-//             x => x in obj
-//         );
-//     }
-
-//     return false;
-// }
-
-// export class FireflyRuntime extends ReactRouterRuntime {
-//     protected readonly _appRouterStore: AppRouterStore;
-//     protected readonly _useMsw: boolean;
-
-//     constructor(options?: FireflyRuntimeOptions);
-//     constructor(members?: FireflyRuntimeMembers);
-
-//     constructor(obj?: FireflyRuntimeOptions | FireflyRuntimeMembers) {
-//         if (isFireflyRuntimeMembers(obj)) {
-//             super(obj);
-
-//             this._appRouterStore = obj.appRouterStore;
-//             this._useMsw = obj.useMsw;
-//         } else {
-//             const {
-//                 plugins,
-//                 useMsw,
-//                 ...options
-//             } = (obj ?? {});
-
-//             if (useMsw) {
-//                 super({
-//                     plugins: [
-//                         ...(plugins ?? []),
-//                         runtime => new MswPlugin(runtime)
-//                     ],
-//                     ...options
-//                 });
-
-//                 this._useMsw = true;
-//             } else {
-//                 super({
-//                     plugins,
-//                     ...options
-//                 });
-
-//                 this._useMsw = false;
-//             }
-
-//             this._appRouterStore = createAppRouterStore(this._logger);
-//         }
-//     }
-
-//     registerRequestHandlers(handlers: RequestHandler[]) {
-//         const mswPlugin = this.getPlugin(MswPluginName) as MswPlugin;
-
-//         if (!mswPlugin) {
-//             throw new Error("[squide] Cannot register the provided MSW request handlers because the runtime hasn't been initialized with MSW. Did you instanciate the FireflyRuntime with the \"useMsw\" option?");
-//         }
-
-//         if (getAreModulesRegistered()) {
-//             throw new Error("[squide] Cannot register an MSW request handlers once the modules are registered. Are you trying to register an MSW request handler in a deferred registration function? Only navigation items can be registered in a deferred registration function.");
-//         }
-
-//         mswPlugin.registerRequestHandlers(handlers);
-//     }
-
-//     // Must define a return type otherwise we get an "error TS2742: The inferred type of 'requestHandlers' cannot be named" error.
-//     get requestHandlers(): RequestHandler[] {
-//         const mswPlugin = this.getPlugin(MswPluginName) as MswPlugin;
-
-//         if (!mswPlugin) {
-//             throw new Error("[squide] Cannot retrieve MSW request handlers because the runtime hasn't been initialized with MSW. Did you instanciate the FireflyRuntime with the \"useMsw\" option?");
-//         }
-
-//         return mswPlugin.requestHandlers;
-//     }
-
-//     registerRoute(route: Route, options: RegisterRouteOptions = {}) {
-//         if (getAreModulesRegistered()) {
-//             throw new Error("[squide] Cannot register a route once the modules are registered. Are you trying to register a route in a deferred registration function? Only navigation items can be registered in a deferred registration function.");
-//         }
-
-//         super.registerRoute(route, options);
-//     }
-
-//     get appRouterStore() {
-//         return this._appRouterStore;
-//     }
-
-//     get isMswEnabled() {
-//         return this._useMsw;
-//     }
-
-//     startScope(logger: Logger): FireflyRuntime {
-//         return new FireflyRuntime({
-//             mode: this._mode,
-//             logger,
-//             eventBus: this._eventBus,
-//             plugins: this._plugins,
-//             routeRegistry: this._routeRegistry,
-//             navigationItemRegistry: this._navigationItemRegistry,
-//             navigationItemScope: this._navigationItemScope,
-//             appRouterStore: this._appRouterStore,
-//             useMsw: this._useMsw
-//         });
-//     }
-// }
-
 export interface RegisterRequestHandlersOptions extends RuntimeMethodOptions {}
 
 export interface IFireflyRuntime extends IReactRouterRuntime {
@@ -222,7 +103,7 @@ export class FireflyRuntimeScope<TRuntime extends FireflyRuntime = FireflyRuntim
         });
     }
 
-    get requestHandlers() {
+    get requestHandlers(): RequestHandler[] {
         return this._runtime.requestHandlers;
     }
 
