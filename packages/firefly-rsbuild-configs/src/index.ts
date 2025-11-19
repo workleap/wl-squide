3c43c17ae8f46d312e00bdc4f52c07c77127c2d6
patricklafrance <pa.lafrance@gmail.com>
2025-11-18 23:33:36 -0500

    refactor(index.ts): replace wildcard export with named exports for clarity and maintainability

--- packages/firefly-rsbuild-configs/src/index.ts
+++ packages/firefly-rsbuild-configs/src/index.ts
@@ -1,2 +1,20 @@
-export * from "./defineConfig.ts";
+export {
+    defineBuildHostConfig,
+    defineBuildRemoteModuleConfig,
+    defineDevHostConfig,
+    defineDevRemoteModuleConfig,
+    type DefineBuildHostConfigOptions,
+    type DefineBuildRemoteModuleConfigOptions,
+    type DefineDevHostConfigOptions,
+    type DefineDevRemoteModuleConfigOptions,
+    type DefineModuleFederationPluginOptions,
+    type Features,
+    type ModuleFederationPluginFactory,
+    type ModuleFederationPluginOptions,
+    type ModuleFederationRemotesOption,
+    type ModuleFederationRuntimePlugins,
+    type ModuleFederationShared,
+    type RemoteDefinition,
+    type Router
+} from "./defineConfig.ts";
 
