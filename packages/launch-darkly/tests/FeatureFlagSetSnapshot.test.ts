8688cc7f54f8949770a5412e8afe89874f0e07fc
patricklafrance <pa.lafrance@gmail.com>
2025-12-05 13:09:30 -0500

    feat(tests): add comprehensive tests for FeatureFlagSetSnapshot and setup vitest configuration

---
+++ packages/launch-darkly/tests/FeatureFlagSetSnapshot.test.ts
@@ -0,0 +1,121 @@
+import { test, vi } from "vitest";
+import { FeatureFlagSetSnapshot } from "../src/FeatureFlagSetSnapshot.ts";
+import { InMemoryLaunchDarklyClient, LaunchDarklyClientNotifier } from "../src/InMemoryLaunchDarklyClient.ts";
+
+test.concurrent("initially set the client flags as the current snapshot", ({ expect }) => {
+    const initialFlags = new Map<string, boolean>(Object.entries({
+        "flag-a": true
+    }));
+
+    const client = new InMemoryLaunchDarklyClient(initialFlags);
+    const snapshot = new FeatureFlagSetSnapshot(client);
+
+    expect(snapshot.value).toEqual(Object.fromEntries(initialFlags));
+});
+
+test.concurrent("when the flags are retrieved twice, the same snapshot object is returned", ({ expect }) => {
+    const initialFlags = new Map<string, boolean>(Object.entries({
+        "flag-a": true
+    }));
+
+    const client = new InMemoryLaunchDarklyClient(initialFlags);
+    const snapshot = new FeatureFlagSetSnapshot(client);
+
+    const value1 = snapshot.value;
+    const value2 = snapshot.value;
+
+    expect(value1).toBe(value2);
+});
+
+test.concurrent("when the client flags change, the snapshot is updated", ({ expect }) => {
+    const initialFlags = new Map<string, boolean>(Object.entries({
+        "flag-a": true
+    }));
+
+    const notifier = new LaunchDarklyClientNotifier();
+
+    const client = new InMemoryLaunchDarklyClient(initialFlags, {
+        notifier
+    });
+
+    const snapshot = new FeatureFlagSetSnapshot(client);
+
+    const value1 = snapshot.value;
+
+    initialFlags.set("flag-b", false);
+
+    notifier.notify("change", {
+        "flag-b": false
+    });
+
+    const value2 = snapshot.value;
+
+    expect(value1).not.toBe(value2);
+    expect(value2["flag-b"]).toBeFalsy();
+});
+
+test.concurrent.only("can register listeners", ({ expect }) => {
+    const initialFlags = new Map<string, boolean>(Object.entries({
+        "flag-a": true
+    }));
+
+    const notifier = new LaunchDarklyClientNotifier();
+
+    const client = new InMemoryLaunchDarklyClient(initialFlags, {
+        notifier
+    });
+
+    const snapshot = new FeatureFlagSetSnapshot(client);
+
+    const listener1 = vi.fn();
+    const listener2 = vi.fn();
+
+    snapshot.addSnapshotChangedListener(listener1);
+    snapshot.addSnapshotChangedListener(listener2);
+
+    initialFlags.set("flag-b", false);
+
+    const changes = {
+        "flag-b": false
+    };
+
+    notifier.notify("change", changes);
+
+    expect(listener1).toHaveBeenCalledExactlyOnceWith(expect.objectContaining(changes), changes);
+    expect(listener2).toHaveBeenCalledExactlyOnceWith(expect.objectContaining(changes), changes);
+});
+
+test.concurrent.only("can remove listeners", ({ expect }) => {
+    const initialFlags = new Map<string, boolean>(Object.entries({
+        "flag-a": true
+    }));
+
+    const notifier = new LaunchDarklyClientNotifier();
+
+    const client = new InMemoryLaunchDarklyClient(initialFlags, {
+        notifier
+    });
+
+    const snapshot = new FeatureFlagSetSnapshot(client);
+
+    const listener1 = vi.fn();
+    const listener2 = vi.fn();
+
+    snapshot.addSnapshotChangedListener(listener1);
+    snapshot.addSnapshotChangedListener(listener2);
+
+    snapshot.removeSnapshotChangedListener(listener1);
+    snapshot.removeSnapshotChangedListener(listener2);
+
+    initialFlags.set("flag-b", false);
+
+    const changes = {
+        "flag-b": false
+    };
+
+    notifier.notify("change", changes);
+
+    expect(listener1).not.toHaveBeenCalled();
+    expect(listener2).not.toHaveBeenCalled();
+});
+
