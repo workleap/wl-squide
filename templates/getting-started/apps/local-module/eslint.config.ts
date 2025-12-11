d73238c78cad857640258d1201ce971635d18eb1
Patrick Lafrance <patricklafrance@users.noreply.github.com>
2025-12-11 00:41:21 -0500

    chore: Update templates (#358)

--- templates/getting-started/apps/local-module/eslint.config.ts
+++ templates/getting-started/apps/local-module/eslint.config.ts
@@ -1,3 +1,5 @@
 import { defineReactLibraryConfig } from "@workleap/eslint-configs";
 
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
 export default defineReactLibraryConfig(import.meta.dirname);
